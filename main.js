d22d6ecd0c72d52c7791db4db36d28e5b9a90a56
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-12-16 15:21:51 -0500

    adds code

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
+// const input1= process.argv[2]
+let data = JSON.stringify(users, null, 2)
 
 let rawData = fs.readFile('./users.json', (err, data) =>{
+if (err) throw err;
 console.log(data)
-})
 let users = JSON.parse(rawData)
+
 console.log(users)
+})
