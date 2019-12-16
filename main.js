cf2932d6b90a3cc4c0601dc42fcd9105bcce32ad
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-16 14:57:31 -0500

    fixes console.log

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-fs.readFile('./test/json', (err, data) =>{
+fs.readFile('./users.json', (err, data) =>{
 console.log(data)
 })
-process.argv(fs.GET users.json)
+// process.argv(fs.GET users.json)
